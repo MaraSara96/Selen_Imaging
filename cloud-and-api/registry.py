4ebfaa19a9f69db7a6dae7ad82ac1e98aded713b
Torsten Weindl <torsten.weindl@gmail.com>
2025-08-25 16:45:23 +0200

    Saving cloud-and-api directory

--- cloud-and-api/registry.py
+++ cloud-and-api/registry.py
@@ -32,3 +32,24 @@ def load_model(stage="Production") -> keras.Model:
         print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
 
         return None
+
+
+def save_model(model: keras.Model = None) -> None:
+    """
+    Save model to GCS
+    """
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    model_path = os.path.join(LOCAL_REGISTRY_PATH, "models", f"{timestamp}.h5")
+    model.save(model_path)
+
+    model_filename = model_path.split("/")[-1]
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(f"models/{model_filename}")
+    blob.upload_from_filename(model_path)
+
+    print("✅ Model saved to GCS")
+
+    return None
